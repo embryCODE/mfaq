74d7f08e0ff438c97714059aa6dbfb450cc3daab
Mason Embry <mason@embrycode.com>
2022-10-16 10:22:59 -0500

    Remove boilerplate and cleanup styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import 'normalize.css/normalize.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-// noinspection JSUnusedGlobalSymbols
 export default MyApp;
